fa7942cf5eab164d461c9fcea5a209cefacd0842
Ayomide Ojelade <ojeladea@outlook.com>
2022-01-28 15:20:12 +0000

    changed p tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>This is the home page</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/clifford.jpg"
       />
     </Layout>
-  )
+  );
 }
 
-export default IndexPage
+export default IndexPage;
