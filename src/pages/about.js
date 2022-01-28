df9d7ccb14e9f8f584ded3807fcb5b68d8f010e8
Dovydas Lozys <reduktorius@gmail.com>
2022-01-28 13:16:41 +0000

    added about page, added links

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Us</title>
+      <h1>About Us</h1>
+      <Link to='/'>Back to Home</Link>
+      <p>Hi there! We're the proud creators of this site, which we built with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
