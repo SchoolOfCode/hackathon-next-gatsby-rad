ccd5b4a6f48e87af71d62212494ec256b45d5e0d
Ayomide Ojelade <ojeladea@outlook.com>
2022-01-28 16:10:35 +0000

    image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
-import { getRandomDogImage } from "../services/dogAPI.js";
+// import { getRandomDogImage } from "../services/dogAPI.js";
 
 function IndexPage() {
   return (
